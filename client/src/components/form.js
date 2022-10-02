903f2c2d378683b22718c7b04871a758bd758ade
Berlin <Berline_lucien@yahoo.com>
2022-10-02 18:16:46 -0400

    added put request notes from cristina

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 const Form = (props) => {
-  const [student, setStudent] = useState({
-    firstname: "",
-    lastname: "",
-  });
+  // const [student, setStudent] = useState({
+  //   firstname: "",
+  //   lastname: "",
+  // });
+  const {initialStudent = {id: null, 
+    firstname: "", 
+  lastname: ""}} = props;
 
+
+// This is the oroginal State with not initial student 
+const [student, setStudent] = useState(initialStudent);
   //create functions that handle the event of the user typing into the form
   const handleNameChange = (event) => {
     const firstname = event.target.value;
@@ -29,13 +35,35 @@ const Form = (props) => {
       })
       .then((data) => {
         console.log("From the post ", data);
-        props.addStudent(data);
+        // props.addStudent(data);
+        props.saveStudent(data);
       });
   };
 
+  //A function to handle the Update request
+  const updateStudent = (existingStudent) =>{
+    return fetch(`http://localhost:8080/api/students/${existingStudent.id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(existingStudent)
+      }).then((response) => {
+          return response.json()
+      }).then((data) => {
+        console.log("From put request ", data);
+        props.saveStudent(data);
+    });
+
+}
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    postStudent(student);
+    //postStudent(student);
+    if(student.id){
+      updateStudent(student);
+    } else{
+      postStudent(student);
+    }
+
   };
 
   return (
@@ -47,7 +75,8 @@ const Form = (props) => {
           id="add-user-name"
           placeholder="First Name"
           required
-          value={student.name}
+          // value={student.name}
+          value={student.firstname}
           onChange={handleNameChange}
         />
         <label>Last Name</label>
@@ -56,11 +85,13 @@ const Form = (props) => {
           id="add-user-lastname"
           placeholder="Last Name"
           required
+          // value={student.lastname}
           value={student.lastname}
           onChange={handleLastnameChange}
         />
       </fieldset>
-      <button type="submit">Add</button>
+      <button type="submit">{!student.id ? "ADD": "SAVE"}</button>
+      {/* <button type="submit">Add</button> */}
     </form>
   );
 };
--- server/server.js
+++ server/server.js
@@ -48,6 +48,28 @@ app.post('/api/students', cors(), async (req, res) => {
   res.json(result.rows[0]);
 });
 
+//A put request - Update a student 
+app.put('/api/students/:studentId', cors(), async (req, res) =>{
+  console.log(req.params);
+  //This will be the id that I want to find in the DB - the student to be updated
+  const studentId = req.params.studentId
+  const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname}
+  console.log("In the server from the url - the student id", studentId);
+  console.log("In the server, from the react - the student to be edited", updatedStudent);
+  // UPDATE students SET lastname = "something" WHERE id="16";
+  const query = `UPDATE students SET lastname=$1, firstname=$2 WHERE id=${studentId} RETURNING *`;
+  const values = [updatedStudent.lastname, updatedStudent.firstname];
+  try {
+    const updated = await db.query(query, values);
+    console.log(updated.rows[0]);
+    res.send(updated.rows[0]);
+
+  }catch(e){
+    console.log(e);
+    return res.status(400).json({e})
+  }
+})
+
 // console.log that your server is up and running
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
